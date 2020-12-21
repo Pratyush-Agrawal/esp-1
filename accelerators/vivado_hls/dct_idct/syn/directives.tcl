97347de5fe5f8afebaa193f8fb709e42b843d379
Pratyush Agrawal <pa2562@columbia.edu>
2020-12-21 10:06:11 -0500

    Hardware files for dct_idct accelerator

---
+++ accelerators/vivado_hls/dct_idct/syn/directives.tcl
@@ -0,0 +1,13 @@
+# Copyright (c) 2011-2019 Columbia University, System Level Design Group
+# SPDX-License-Identifier: Apache-2.0
+
+# User-defined configuration ports
+# <<--directives-param-->>
+set_directive_interface -mode ap_none "top" conf_info_input_cols
+set_directive_interface -mode ap_none "top" conf_info_input_rows
+set_directive_interface -mode ap_none "top" conf_info_dct_istrue
+set_directive_interface -mode ap_none "top" conf_info_output_rows
+set_directive_interface -mode ap_none "top" conf_info_output_cols
+
+# Insert here any custom directive
+
